600e2b201f4be25d3404b2e23124be4eacf54aae
mflojo <m.eric.flojo@gmail.com>
2025-02-16 10:15:47 -0800

    Create library array, book functions

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
